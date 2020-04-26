744609791546a33826c9e2f1781b4290aacc2c97
Cevad Tokatli <cevadtokatli@hotmail.com>
2020-04-26 21:27:41 +0300

    add default value

---
+++ src/default-value/index.tsx
@@ -0,0 +1,38 @@
+const DV : {
+    [key: string]: any
+} = {
+    // color
+    BLUE: "#0665d0",
+    INDIGO: "#675aa9",
+    PURPLE: "#6658dd",
+    PINK: "#f672a7",
+    RED: "#f1556c",
+    ORANGE: "#fd7e14",
+    YELLOW: "#f7b84b",
+    GREEN: "#1abc9c",
+    TEAL: "#02a8b5",
+    CYAN: "#4fc6e1",
+    WHITE: "#fff",
+    GRAY_100: "#f1f5f7",
+    GRAY_200: "#eef0f3",
+    GRAY_300: "#dee2e6",
+    GRAY_400: "#ced4da",
+    GRAY_500: "#b1bbc4",
+    GRAY_600: "#98a6ad",
+    GRAY_700: "#6c757d",
+    GRAY_800: "#343a40",
+    GRAY_900: "#323a46",
+    BLACK: "#000",
+    LIGHT_FONT_COLOR: "#fff",
+    DARK_FONT_COLOR: "#000",
+
+    // util
+    DARK_LEVEL: .075,
+    DARKER_LEVEL: .175,
+    LIGHT_LEVEL: .075,
+    LIGHTER_LEVEL: .175,
+    SHADOW_FADE_LEVEL: .5,
+    JSX_ELEMENT_PROPS: {}
+}
+
+export default DV
