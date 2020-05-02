b799e607169d4758b5d83e34e69bc04af266cf9c
Cevad Tokatli <cevadtokatli@hotmail.com>
2020-05-03 02:55:46 +0300

    add checkbox-size

---
+++ src/constants/form/checkbox-size.tsx
@@ -0,0 +1,49 @@
+export default {
+    rectangle: {
+        small: "12px",
+        default: "16px",
+        large: "20px"
+    },
+    stickContainer: {
+        small: {
+            left: "1px",
+            top: "1.5px"
+        },
+        default: {
+            left: "4px",
+            top: "4px"
+        },
+        large: {
+            left: "7.25px",
+            top: "6px"
+        }
+    },
+    shortStick: {
+        small: {
+            top: "2px",
+            height: "3.5px"
+        },
+        default: {
+            top: "1.5px",
+            height: "3.5px"
+        },
+        large: {
+            top: ".5px",
+            height: "5px"
+        }
+    },
+    longStick: {
+        small: {
+            top: "4.5px",
+            width: "6.5px"
+        },
+        default: {
+            top: "5px",
+            width: "9.5px"
+        },
+        large: {
+            top: "5px",
+            width: "12px"
+        }
+    }
+}
