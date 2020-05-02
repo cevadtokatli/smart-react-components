e08a6c117a2390d80af522952ad5ecee68bb1b6b
Cemil Tokatlı <cemiltokatli@yahoo.com>
2020-05-03 02:59:14 +0300

    add form/InputWrapper

---
+++ src/form/InputWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export default styled.div`
+    position: relative;
+    display: flex;
+    user-select: none;
+
+    > input {
+        height: 100%;
+    }
+`
