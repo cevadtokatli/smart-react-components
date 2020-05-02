dd6510023b5753d37093d4279428a6f42e1a0a05
Cevad Tokatli <cevadtokatli@hotmail.com>
2020-05-03 02:38:48 +0300

    add icons

---
+++ src/icon/ArrowDown.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styled from "styled-components"
+import Svg from "../element/Svg"
+
+export default styled(Svg).attrs({
+    children: <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>,
+    viewBox: "5 4 14 14"
+})``
