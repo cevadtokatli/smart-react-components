ba2e6a840817daa81e9eab2d1a1a64341bb17801
Cevad Tokatli <cevadtokatli@hotmail.com>
2020-05-03 02:56:45 +0300

    add switch-size

---
+++ src/constants/form/switch-size.tsx
@@ -0,0 +1,30 @@
+export default {
+    foundation: {
+        small: {
+            width: "22px",
+            height: "12px"
+        },
+        default: {
+            width: "28px",
+            height: "16px"
+        },
+        large: {
+            width: "34px",
+            height: "20px"
+        }
+    },
+    slider: {
+        small: {
+            size: "10px",
+            left: "10px"
+        },
+        default: {
+            size: "14px",
+            left: "12px"
+        },
+        large: {
+            size: "18px",
+            left: "14px"
+        }
+    }
+}
