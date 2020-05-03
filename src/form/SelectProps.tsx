6e4498ea1e183b21b71bc7b3cc000fd2a284ef8f
Cevad Tokatli <cevadtokatli@hotmail.com>
2020-05-03 03:36:41 +0300

    add form/Select

---
+++ src/form/SelectProps.tsx
@@ -0,0 +1,27 @@
+import {SizeProps} from "../props/size-props"
+import {JSXElementProps, JSXChild, ValueMix, SetValue} from "../props"
+
+export default interface SelectProps extends SizeProps {
+    containerProps?: JSXElementProps
+    selectProps?: JSXElementProps
+    labelProps?: JSXElementProps
+    label?: JSXChild
+    children?: JSX.Element|JSX.Element[]
+    optionType?: string
+    active: ValueMix
+    setActive: SetValue<ValueMix>
+    type?: string
+    fill?: boolean
+    border?: boolean
+    shape?: string
+    hover?: boolean
+    error?: JSXChild
+    leftAddon?: JSXChild
+    leftAddonProps?: JSXElementProps
+    leftAddonAbsolute?: boolean
+    rightAddon?: JSXChild
+    rightAddonProps?: JSXElementProps
+    rightAddonAbsolute?: boolean
+    search?: boolean
+    searchInputProps?: JSXElementProps
+}
