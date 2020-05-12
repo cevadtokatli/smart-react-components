5e17c9bbeb0650bd59191cd1fea30c145880346f
Cevad Tokatli <cevadtokatli@hotmail.com>
2020-05-13 01:10:50 +0300

    add alert/IconElement

---
+++ src/alert/IconElement.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default styled.div`
+    flex: 0 0 auto;
+    align-self: stretch;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
