3ec1c54af3650cdea40ab60f52aa19cbd6728c04
Cemil Tokatlı <cemiltokatli@yahoo.com>
2020-05-21 00:24:45 +0300

    add types/pop-up

---
+++ src/types/pop-up.tsx
@@ -0,0 +1,13 @@
+export interface AlertLanguage {
+    close: string
+}
+
+export interface ConfirmLangauge {
+    cancel: string
+    ok: string
+}
+
+export interface PromptLangauge {
+    cancel: string
+    ok: string
+}
