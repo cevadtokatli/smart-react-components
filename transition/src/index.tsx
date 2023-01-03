201f703d6e24fc941f8d465f62aae75e5b924b61
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-04 02:13:05 +0300

    Add CSSTransitionSwitch

--- transition/src/index.tsx
+++ transition/src/index.tsx
@@ -1,3 +1,4 @@
 export { default as CSSTransition } from './CSSTransition'
+export { default as CSSTransitionSwitch } from './CSSTransitionSwitch'
 export { default as Transition } from './Transition'
 export { default as TransitionSwitch } from './TransitionSwitch'
