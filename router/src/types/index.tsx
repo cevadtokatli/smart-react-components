dcd5b0c7162b71fd097bb5151792fcb9bed7ef8e
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-05 01:04:31 +0300

    Add router types

---
+++ router/src/types/index.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react'
+
+type Get = (
+  match: Match,
+  url: URL,
+  setPercentage: (payload: number) => void,
+  setCancelCallback: (payload: () => void) => void
+) => Promise<void>
+
+export interface Match {
+  isExact: boolean
+  params: {
+    [key: string]: string | number
+  }
+  path: string
+  url: string
+}
+
+type Module = () => Promise<{
+  default: ReactNode
+  get: Get
+}>
+
+export type Path = string | RegExp
+
+export interface RouteModule {
+  children?: RouteModule[]
+  module: Module
+  path: Path
+}
+
+export interface URL {
+  fullpath: string
+  params: string[]
+  pathname: string
+  search: string
+  query: {
+    [key: string]: string
+  }
+}
