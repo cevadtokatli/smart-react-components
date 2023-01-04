846b06bef7b36cee77a753552ae4921cc7d4a1c1
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-04 03:18:13 +0300

    Add CSSTransitionGroup

---
+++ transition/src/CSSTransitionGroup.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import CSSTransitionGroup from './CSSTransitionGroup'
+
+describe('<CSSTransitionGroup />', () => {
+  it('should render all children elements', () => {
+    const screen = render(<CSSTransitionGroup className="test" duration={100}><div key="1" /><div key="2" /></CSSTransitionGroup>)
+    expect(screen.container.querySelectorAll('div').length).toEqual(2)
+  })
+})
---
+++ transition/src/CSSTransitionGroup.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import CSSTransition from './CSSTransition'
+import useTransitionGroup from './hooks/useTransitionGroup'
+import { CSSTransitionProps, TransitionGroupProps } from './types'
+
+export interface Props extends CSSTransitionProps, TransitionGroupProps {}
+
+const CSSTransitionGroup: React.FC<Props> = ({ afterHide, afterShow, beforeHide, beforeShow, children, className, duration, hasHideAnimation, hasShowAnimation }) => {
+  const { elements, handleAfterHide, keys } = useTransitionGroup({ afterHide, children })
+
+  return (
+    <>
+      { elements.map(element => (
+        <CSSTransition
+          afterHide={handleAfterHide}
+          afterShow={afterShow}
+          beforeHide={beforeHide}
+          beforeShow={beforeShow}
+          className={className}
+          duration={duration}
+          hasHideAnimation={hasHideAnimation}
+          hasShowAnimation={hasShowAnimation}
+          key={element.key}
+          status={!keys.includes(element.key)}
+        >
+          { element }
+        </CSSTransition>
+      )) }
+    </>
+  )
+}
+
+export default CSSTransitionGroup
