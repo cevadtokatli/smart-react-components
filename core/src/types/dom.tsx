f701872227530faa33c12e44e1bfc5c13007cff4
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-06 00:16:24 +0300

    Add add remove event methods

---
+++ core/src/types/dom.tsx
@@ -0,0 +1,7 @@
+export type EventAddOptions = boolean | AddEventListenerOptions
+
+export type EventCallback = (e: Event) => void
+
+export type EventElement = HTMLElement | Document | Window
+
+export type EventRemoveOptions = boolean | EventListenerOptions
--- core/src/util/dom.tsx
+++ core/src/util/dom.tsx
@@ -1,3 +1,5 @@
+import { EventAddOptions, EventCallback, EventElement, EventRemoveOptions } from '../types/dom'
+
 export const isServer = typeof window === 'undefined'
 
 export const requestAnimationFrame: ((callback:(() => void)) => void) = !isServer
@@ -8,3 +10,33 @@ export const requestAnimationFrame: ((callback:(() => void)) => void) = !isServe
   || (window as any).msRequestAnimationFrame
   || ((callback: Function) => { window.setTimeout(callback, 1000 / 60) })
   : () => {}
+
+/**
+ * Attaches events to the given element.
+ */
+export const addEventListener = (el: EventElement, events: string[], callback: EventCallback, options: EventAddOptions = true) => {
+  for (const i in events) {
+    el.addEventListener(events[i], callback, options)
+  }
+}
+
+/**
+ * Attaches events to the given element for once.
+ */
+export const addEventListenerOnce = (el: EventElement, events: string[], callback: EventCallback, addOptions: EventAddOptions = true, removeOptions: EventRemoveOptions = true) => {
+  const cb: EventListener = (e: Event) => {
+    removeEventListener(el, events, cb, removeOptions)
+    callback(e)
+  }
+
+  addEventListener(el, events, cb, addOptions)
+}
+
+/**
+ * Removes events from the given element.
+ */
+export const removeEventListener = (el: EventElement, events: string[], callback: EventCallback, options: EventRemoveOptions = true) => {
+  for (const i in events) {
+    el.removeEventListener(events[i], callback, options)
+  }
+}
