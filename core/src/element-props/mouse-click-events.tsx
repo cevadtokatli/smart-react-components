4d0dbcd9ff3e73cda4ff2a40bb5bd45b1465c214
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-05 11:40:20 +0300

    Fix for mouse and touch events responsive

--- core/src/element-props/mouse-click-events.tsx
+++ core/src/element-props/mouse-click-events.tsx
@@ -1,21 +1,19 @@
 import { MouseEvent } from 'react'
-import { includeResponsiveProps } from './'
-import { ResponsiveProps } from '../types'
 
-export type MouseClickEvents = ResponsiveProps<{
+export interface MouseClickEvents {
   onClick?: (e: MouseEvent<HTMLElement>) => void
   onContextMenu?: (e: MouseEvent<HTMLElement>) => void
   onDblClick?: (e: MouseEvent<HTMLElement>) => void
   onMouseDown?: (e: MouseEvent<HTMLElement>) => void
   onMouseUp?: (e: MouseEvent<HTMLElement>) => void
-}>
+}
 
-const mouseClickEvents = includeResponsiveProps([
+const mouseClickEvents = [
   'onClick',
   'onContextMenu',
   'onDblClick',
   'onMouseDown',
   'onMouseUp',
-])
+]
 
 export default mouseClickEvents
--- core/src/element-props/touch-click-events.tsx
+++ core/src/element-props/touch-click-events.tsx
@@ -1,15 +1,13 @@
 import { TouchEvent } from 'react'
-import { includeResponsiveProps } from './'
-import { ResponsiveProps } from '../types'
 
-export type TouchClickEvents = ResponsiveProps<{
+export interface TouchClickEvents {
   onTouchEnd?: (e: TouchEvent<HTMLElement>) => void
   onTouchStart?: (e: TouchEvent<HTMLElement>) => void
-}>
+}
 
-const touchClickEvents = includeResponsiveProps([
+const touchClickEvents = [
   'onTouchEnd',
   'onTouchStart',
-])
+]
 
 export default touchClickEvents
