93aee61d59bcb58a1ac87b2420fc985d4f26450a
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-29 04:35:49 +0300

    Update Input playground

--- playground/src/components/Input.tsx
+++ playground/src/components/Input.tsx
@@ -1,5 +1,4 @@
 import { Input } from "@smart-react-components/ui"
-import InputAddon, { Position } from '@smart-react-components/ui/Input/InputAddon'
 import React from 'react'
 
 export const Palette = () => {
@@ -7,68 +6,68 @@ export const Palette = () => {
 
   return (
     <>
-      <Input placeholder="Primary" value={value} setValue={setValue}>Primary</Input>
-      <Input palette="secondary" placeholder="Secondary">Secondary</Input>
-      <Input palette="success" placeholder="Success">Success</Input>
-      <Input palette="danger" placeholder="Danger">Danger</Input>
-      <Input palette="warning" placeholder="Warning">Warning</Input>
-      <Input palette="info" placeholder="Info">Info</Input>
-      <Input palette="dynamic" placeholder="Dynamic">Dynamic</Input>
-      <Input palette="!dynamic" placeholder="!Dynamic">!Dynamic</Input>
+      <Input placeholder="Primary" value={value} setValue={setValue} label="Primary" />
+      <Input palette="secondary" placeholder="Secondary" label="Secondary" />
+      <Input palette="success" placeholder="Success" label="Success" />
+      <Input palette="danger" placeholder="Danger" label="Danger" />
+      <Input palette="warning" placeholder="Warning" label="Warning" />
+      <Input palette="info" placeholder="Info" label="Info" />
+      <Input palette="dynamic" placeholder="Dynamic" label="Dynamic" />
+      <Input palette="!dynamic" placeholder="!Dynamic" label="!Dynamic" />
     </>
   )
 }
 
 export const Soft = () => (
   <>
-    <Input isSoft>Primary</Input>
-    <Input palette="secondary" isSoft>Secondary</Input>
-    <Input palette="success" isSoft>Success</Input>
-    <Input palette="danger" isSoft>Danger</Input>
-    <Input palette="warning" isSoft>Warning</Input>
-    <Input palette="info" isSoft>Info</Input>
-    <Input palette="dynamic" isSoft>Dynamic</Input>
-    <Input palette="!dynamic" isSoft>!Dynamic</Input>
+    <Input placeholder="Primary" label="Primary" isSoft />
+    <Input palette="secondary" placeholder="Secondary" label="Secondary" isSoft />
+    <Input palette="success" placeholder="Success" label="Success" isSoft />
+    <Input palette="danger" placeholder="Danger" label="Danger" isSoft />
+    <Input palette="warning" placeholder="Warning" label="Warning" isSoft />
+    <Input palette="info" placeholder="Info" label="Info" isSoft />
+    <Input palette="dynamic" placeholder="Dynamic" label="Dynamic" isSoft />
+    <Input palette="!dynamic" placeholder="!Dynamic" label="!Dynamic" isSoft />
   </>
 )
 
 export const Outline = () => (
   <>
-    <Input isOutline={false} placeholder="Primary">Primary</Input>
-    <Input palette="secondary" isOutline={false} placeholder="Secondary">Secondary</Input>
-    <Input palette="success" isOutline={false} placeholder="Success">Success</Input>
-    <Input palette="danger" isOutline={false} placeholder="Danger">Danger</Input>
-    <Input palette="warning" isOutline={false} placeholder="Warning">Warning</Input>
-    <Input palette="info" isOutline={false} placeholder="Info">Info</Input>
-    <Input palette="dynamic" isOutline={false} placeholder="Dynamic">Dynamic</Input>
-    <Input palette="!dynamic" isOutline={false} placeholder="!Dynamic">!Dynamic</Input>
+    <Input placeholder="Primary" label="Primary" isOutline={false} />
+    <Input palette="secondary" placeholder="Secondary" label="Secondary" isOutline={false} />
+    <Input palette="success" placeholder="Success" label="Success" isOutline={false} />
+    <Input palette="danger" placeholder="Danger" label="Danger" isOutline={false} />
+    <Input palette="warning" placeholder="Warning" label="Warning" isOutline={false} />
+    <Input palette="info" placeholder="Info" label="Info" isOutline={false} />
+    <Input palette="dynamic" placeholder="Dynamic" label="Dynamic" isOutline={false} />
+    <Input palette="!dynamic" placeholder="!Dynamic" label="!Dynamic" isOutline={false} />
   </>
 )
 
 export const OutlineSoft = () => (
   <>
-    <Input isOutline={false} isSoft placeholder="Primary">Primary</Input>
-    <Input palette="secondary" isOutline={false} isSoft placeholder="Secondary">Secondary</Input>
-    <Input palette="success" isOutline={false} isSoft placeholder="Success">Success</Input>
-    <Input palette="danger" isOutline={false} isSoft placeholder="Danger">Danger</Input>
-    <Input palette="warning" isOutline={false} isSoft placeholder="Warning">Warning</Input>
-    <Input palette="info" isOutline={false} isSoft placeholder="Info">Info</Input>
-    <Input palette="dynamic" isOutline={false} isSoft placeholder="Dynamic">Dynamic</Input>
-    <Input palette="!dynamic" isOutline={false} isSoft placeholder="!Dynamic">!Dynamic</Input>
+    <Input placeholder="Primary" label="Primary" isSoft isOutline={false} />
+    <Input palette="secondary" placeholder="Secondary" label="Secondary" isSoft isOutline={false} />
+    <Input palette="success" placeholder="Success" label="Success" isSoft isOutline={false} />
+    <Input palette="danger" placeholder="Danger" label="Danger" isSoft isOutline={false} />
+    <Input palette="warning" placeholder="Warning" label="Warning" isSoft isOutline={false} />
+    <Input palette="info" placeholder="Info" label="Info" isSoft isOutline={false} />
+    <Input palette="dynamic" placeholder="Dynamic" label="Dynamic" isSoft isOutline={false} />
+    <Input palette="!dynamic" placeholder="!Dynamic" label="!Dynamic" isSoft isOutline={false} />
   </>
 )
 
 export const Size = () => (
   <>
-    <Input size="small">Small</Input>
-    <Input>Medium</Input>
-    <Input size="large">Large</Input>
+    <Input size="small" label="Small" />
+    <Input label="Medium" />
+    <Input size="large" label="Large" />
   </>
 )
 
 export const Shape = () => (
   <>
-    <Input shape="rectangle">Rectangle</Input>
-    <Input shape="rounded">Rounded</Input>
+    <Input shape="rectangle" label="Rectangle" />
+    <Input shape="rounded" label="Rounded" />
   </>
 )
