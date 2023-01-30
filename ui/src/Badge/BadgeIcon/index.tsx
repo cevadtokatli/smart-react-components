4b6fd605fe8a8e7de2744e02adef67e0447a41cc
cevadtokatli <cevadtokatli@hotmail.com>
2023-01-30 06:29:00 +0300

    Fix for mouseEvents not attached to BadgeIcon

--- ui/src/Badge/BadgeIcon/index.tsx
+++ ui/src/Badge/BadgeIcon/index.tsx
@@ -1,4 +1,5 @@
-import { ClickEvents } from '@smart-react-components/core/element-props/click-events'
+import extractElementProps from '@smart-react-components/core/element-props'
+import clickEvents, { ClickEvents } from '@smart-react-components/core/element-props/click-events'
 import mouseClickEvents from '@smart-react-components/core/element-props/mouse-click-events'
 import { PaletteProp, ShapeProp } from '@smart-react-components/core/types'
 import React from 'react'
@@ -18,6 +19,7 @@ interface PrivateProps {
 }
 const BadgeIcon: React.FC<Props> = (props: Props & PrivateProps) => (
   <BadgeIconElement
+    {...extractElementProps(props, [clickEvents])}
     iconPosition={props.iconPosition}
     isClickable={!!mouseClickEvents.find(i => props[i])}
     isOutline={props.isOutline}
