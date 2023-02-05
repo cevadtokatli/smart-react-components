75e407afe9110e41e89a1be1e5d39a2fd06308a9
cevadtokatli <cevadtokatli@hotmail.com>
2023-02-05 06:22:43 +0300

    Optimize Loading

--- ui/src/Loading/index.tsx
+++ ui/src/Loading/index.tsx
@@ -2,7 +2,7 @@ import extractElementProps from '@smart-react-components/core/element-props'
 import intrinsicStyledMarginProps, { IntrinsicStyledMarginProps } from '@smart-react-components/core/element-props/intrinsic-styled-margin-props'
 import { SizeProps } from '@smart-react-components/core/types'
 import React from 'react'
-import LoadingElement from './LoadingElement'
+import LoadingElement from '../components/Loading/LoadingElement'
 
 export interface Props extends IntrinsicStyledMarginProps, SizeProps {
   color?: string
