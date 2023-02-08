d45eec36b00c64bae12b94a65f3306c88fac95b3
cevadtokatli <cevadtokatli@hotmail.com>
2023-02-08 03:08:33 +0300

    Add ColorPicker -> ButtonsSection

---
+++ ui/src/components/ColorPicker/ButtonsSection/index.tsx
@@ -0,0 +1,58 @@
+import Section from '@smart-react-components/core/Element/Section'
+import { Theme } from '@smart-react-components/core/theme'
+import { PaletteProp } from '@smart-react-components/core/types'
+import React from 'react'
+import { ThemeContext } from 'styled-components'
+import Button from '../../../Button'
+import ButtonList from '../../../Button/ButtonList'
+import ColorPickerProps from '../../../context/ColorPickerProps'
+import SaveIcon from '../../../icons/Save'
+import CancelIcon from '../../../icons/Cancel'
+
+interface Props {
+  isSoft: boolean
+  cancelLabel?: string
+  saveLabel?: string
+  onCancel: (e: Event) => void
+  onSave: (e: Event) => void
+  palette: PaletteProp
+}
+
+const ButtonsSection: React.FC<Props> = ({ cancelLabel, saveLabel, isSoft, onCancel, onSave, palette }) => {
+  const { colorPickerSize, colorPickerSizeSm, colorPickerSizeMd, colorPickerSizeLg, colorPickerSizeXl } = React.useContext(ColorPickerProps)
+  const theme = React.useContext<Theme>(ThemeContext)
+
+  return (
+    <Section>
+      <ButtonList
+        hasSpace={false}
+        isBlock
+        isSoft={isSoft}
+        palette={palette}
+        size={colorPickerSize}
+        sizeSm={colorPickerSizeSm}
+        sizeMd={colorPickerSizeMd}
+        sizeLg={colorPickerSizeLg}
+        sizeXl={colorPickerSizeXl}
+      >
+        <Button
+          marginRight={`$size.colorPicker.${colorPickerSize}.space`}
+          {...(colorPickerSizeSm && { marginRightSm: `$size.colorPicker.${colorPickerSizeSm}.space` })}
+          {...(colorPickerSizeMd && { marginRightMd: `$size.colorPicker.${colorPickerSizeMd}.space` })}
+          {...(colorPickerSizeLg && { marginRightLg: `$size.colorPicker.${colorPickerSizeLg}.space` })}
+          {...(colorPickerSizeXl && { marginRightXl: `$size.colorPicker.${colorPickerSizeXl}.space` })}
+          onClick={e => onSave(e as any)}
+        >
+          <SaveIcon />
+          <span>{saveLabel ?? theme.$.i18n.save}</span>
+        </Button>
+        <Button onClick={e => onCancel(e as any)}>
+          <CancelIcon />
+          <span>{cancelLabel ?? theme.$.i18n.cancel}</span>
+        </Button>
+      </ButtonList>
+    </Section>
+  )
+}
+
+export default ButtonsSection
