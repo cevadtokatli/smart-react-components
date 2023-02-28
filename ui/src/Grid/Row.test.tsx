9923beed44b20676020959a98f1e7308db44508c
cevadtokatli <cevadtokatli@hotmail.com>
2023-02-28 03:33:30 +0300

    Update snapshots

--- ui/src/Grid/Row.test.tsx
+++ ui/src/Grid/Row.test.tsx
@@ -1,10 +1,11 @@
+import { wrapTheme } from '@smart-react-components/core/test'
 import { render } from '@testing-library/react'
 import React from 'react'
 import Row from './Row'
 
 describe('<Row />', () => {
   it('should render component with default props', () => {
-    const { asFragment } = render(<Row />)
+    const { asFragment } = render(wrapTheme(<Row />))
     expect(asFragment()).toMatchSnapshot()
   })
 })
