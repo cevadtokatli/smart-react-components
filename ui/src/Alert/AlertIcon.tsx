de7279745c1af8b0327538f45b1eb73845546246
cevadtokatli <cevadtokatli@hotmail.com>
2023-05-09 20:43:33 +0300

    Add elementProps to AlertIcon

--- ui/src/Alert/AlertIcon.tsx
+++ ui/src/Alert/AlertIcon.tsx
@@ -1,12 +1,13 @@
 import extractElementProps from '@smart-react-components/core/element-props'
 import clickEvents, { ClickEvents } from '@smart-react-components/core/element-props/click-events'
-import { PaletteProp, ResponsiveProp, SizeProp } from '@smart-react-components/core/types'
+import { JSXElementProps, PaletteProp, ResponsiveProp, SizeProp } from '@smart-react-components/core/types'
 import React from 'react'
 import AlertIconElement from '../components/Alert/AlertIconElement'
 import { OrderPosition } from '../types'
 
 export interface Props extends ClickEvents {
   children: JSX.Element
+  elementProps?: JSXElementProps
   hasBackground?: boolean
   hasHover?: boolean
   palette?: PaletteProp
@@ -34,10 +35,12 @@ const AlertIcon: React.FC<Props> = (props: Props & PrivateProps) => (
     isSoft={props.isSoft}
     palette={props.palette ?? props.alertPalette}
     {...extractElementProps(props, [clickEvents])}
+    {...props.elementProps}
   >{props.children}</AlertIconElement>
 )
 
 AlertIcon.defaultProps = {
+  elementProps: {},
   hasBackground: true,
 }
 
