5702b12b71011ae63acdea0a253c94bc60ecdc4b
cevadtokatli <cevadtokatli@hotmail.com>
2023-06-28 12:55:43 +0300

    Add DatePicker playground

---
+++ playground/src/components/DatePicker.tsx
@@ -0,0 +1,17 @@
+import { Div } from '@smart-react-components/core'
+import { DatePicker } from '@smart-react-components/ui'
+import { DatePickerTimeType } from '@smart-react-components/ui/DatePicker'
+import React from 'react'
+
+export const Palette = () => {
+  const [value, setValue] = React.useState<Date>(new Date())
+
+  return (
+    <Div display="inline-block">
+      <DatePicker
+        setValue={setValue}
+        value={value}
+      />
+    </Div>
+  )
+}
