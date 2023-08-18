d917a7124d448549a3f3df632c95e50b0b916f2b
cevadtokatli <cevadtokatli@hotmail.com>
2023-08-18 15:12:58 +0300

    Update Th text-align

--- ui/src/Table/Th.tsx
+++ ui/src/Table/Th.tsx
@@ -1,3 +1,5 @@
 import Th from '@smart-react-components/core/Element/Th'
+import { StyledProps } from '@smart-react-components/core/styled-props'
+import styled from 'styled-components'
 
-export default Th
+export default styled(Th).attrs<StyledProps>(({ textAlign = 'left' }) => ({ textAlign }))<StyledProps>``
