8489782417404ab9f824f53da5c53aa6deea5631
cevadtokatli <cevadtokatli@hotmail.com>
2023-10-31 13:46:07 +0300

    Prevent Container from overwriting CSS

--- ui/src/Grid/Container.tsx
+++ ui/src/Grid/Container.tsx
@@ -3,15 +3,15 @@ import { StyledProps } from '@smart-react-components/core/styled-props'
 import styled from 'styled-components'
 
 export default styled(Div).attrs<StyledProps>(
-  ({ marginHorizontal = 'auto', paddingHorizontal = '$length.grid.space' }) => ({
-    marginHorizontal,
-    paddingHorizontal,
-    maxWidth: '100%',
-    maxWidthSm: '{length.grid.containerWidth.small}px',
-    maxWidthMd: '{length.grid.containerWidth.medium}px',
-    maxWidthLg: '{length.grid.containerWidth.large}px',
-    maxWidthXl: '{length.grid.containerWidth.xlarge}px',
-  }),
+  ({
+    marginHorizontal = 'auto',
+    paddingHorizontal = '$length.grid.space',
+    maxWidth = '100%',
+    maxWidthSm = '{length.grid.containerWidth.small}px',
+    maxWidthMd = '{length.grid.containerWidth.medium}px',
+    maxWidthLg = '{length.grid.containerWidth.large}px',
+    maxWidthXl = '{length.grid.containerWidth.xlarge}px',
+  }) => ({ marginHorizontal, paddingHorizontal, maxWidth, maxWidthSm, maxWidthMd, maxWidthLg, maxWidthXl }),
 )<StyledProps>`
   box-sizing: border-box;
 `
