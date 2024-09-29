fbc27ceb266b263b212bdf8704c088b171bcdbf1
Cevad Tokatli <cevadtokatli@hotmail.com>
2024-09-29 15:06:48 +0300

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,64 +6,9 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-# Returns the version value from the package.json increasing it by 1.
-function getVersion() {
-  local currentVersion=$(grep -o '"version": "[^"]*' package.json | grep -o '[^"]*$')
-  local currentVersionNumber=$(echo $currentVersion | grep -o '[0-9]*$')
-
-  local newVersionNumber=$((currentVersionNumber + 1))
-  local newVersion=$(echo $currentVersion | sed "s/[0-9]*$/$newVersionNumber/g")
-
-  echo $newVersion
-}
-
-# Updates the given version in the package.json
-function updateVersion() {
-  # params
-  local key=$1
-  local value=$2
-
-  local currentAttr=$(grep -o "\"$key\": \"[^\"\^]*\"," package.json)
-  local currentValue=$(echo $currentAttr | grep -o '[^"]*",$' | grep -o '[^",]*')
-  local newAttr=${currentAttr/$currentValue/$value}
-  sed -i '' "s#$currentAttr#$newAttr#g" package.json
-
-  if grep -q "\"$key\": \"\^[^\"]*\"," package.json ; then
-    local peerCurrentAttr=$(grep -o "\"$key\": \"\^[^\"]*\"," package.json)
-    local peerCurrentValue=$(echo $peerCurrentAttr | grep -o '[^"\^]*",$' | grep -o '[^",]*')
-    local peerNewAttr=${peerCurrentAttr/$peerCurrentValue/$value}
-    sed -i '' "s#$peerCurrentAttr#$peerNewAttr#g" package.json
-  fi
-}
-
-# Updates the released module's dependant modules' package.json to use the current version.
-function updateModuleDependentsPackageJson() {
-  # params
-  local module=$1
-  local version=$2
-
-  cd ../playground
-  updateVersion "@smart-react-components/$module" $version
-
-  if [ $module = "core" ] ; then
-    cd ../transition
-    updateVersion "@smart-react-components/$module" $version
-    cd ../router
-    updateVersion "@smart-react-components/$module" $version
-  fi
-
-  if [ $module != "ui" ] ; then
-    cd ../ui
-    updateVersion "@smart-react-components/$module" $version
-  fi
-
-  cd "../$module"
-}
-
 function release() {
   # params
   local module=$1
-  local shouldUpdateVersion=$2
 
   echo -e "$YELLOW***** Releasing $module *****$NC"
   cd $module
@@ -78,22 +23,11 @@ function release() {
     exit 1
   fi
 
-  if [ "$shouldUpdateVersion" = "true" ] ; then
-    local version=$(getVersion)
-    updateVersion "version" $version
-  fi
-
   npm run build
 
   if [ $? -ne '0' ] ; then
     echo -e "$RED***** The build operation has been failed *****$NC"
     exit 1
-  fi
-
-  if [ "$shouldUpdateVersion" = "true" ] ; then
-    updateModuleDependentsPackageJson $module $version
-    git commit -a -m "Release module \"$module\" to \"$version\""
-    git push -u origin
   fi
 
   cd lib && npm publish
@@ -105,7 +39,6 @@ function release() {
 function main() {
   # params
   local module=$1
-  local shouldUpdateVersion=$2
 
   cd ..
 
@@ -114,15 +47,15 @@ function main() {
     exit 1
   fi
 
-  release $module "$shouldUpdateVersion"
+  release $module
 
   if [ $module = "core" ] ; then
-    release "transition" "$shouldUpdateVersion"
-    release "router" "$shouldUpdateVersion"
+    release "transition"
+    release "router"
   fi
 
   if [ $module != "ui" ] ; then
-    release "ui" "$shouldUpdateVersion"
+    release "ui"
   fi
 
   cd ./playground
